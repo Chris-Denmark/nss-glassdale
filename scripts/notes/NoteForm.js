8d5f37f7ba08b4c7cb8a3097d418057f4a3086cd
Chris-Denmark <cdenmark97@gmail.com>
2021-01-06 15:47:39 -0600

    Added the feature to delete notes

--- scripts/notes/NoteForm.js
+++ scripts/notes/NoteForm.js
@@ -1,4 +1,5 @@
 import { saveNote } from "./NoteDataProvider.js"
+import { useCriminals, getCriminals } from "../criminals/CriminalDataProvider.js"
 
 const contentTarget = document.querySelector(".noteFormContainer")
 const eventHub = document.querySelector(".container")
@@ -7,12 +8,12 @@ eventHub.addEventListener("click", clickEvent => {
     if (clickEvent.target.id === "saveNote") {
         const author = document.querySelector("#author").value
         const text = document.querySelector("#text").value
-        const suspect = document.querySelector("#suspect").value
+        const criminalId = parseInt(document.querySelector("#suspect").value)
 
         const newNote = {
            author: author,
            text: text,
-           suspect: suspect,
+           criminalId: criminalId,
            timestamp: Date.now()
         }
         saveNote(newNote)
@@ -21,14 +22,26 @@ eventHub.addEventListener("click", clickEvent => {
 
 
 const render = () => {
+    const criminalsCollection = useCriminals()
+
     contentTarget.innerHTML = `
         <input type="text" id="author" placeholder="author name">
         <textarea id="text" placeholder="note text"></textarea>
-        <input type="text" id="suspect" placeholder="suspect name">
+        <select class="dropdown" id="suspect">
+            <option value="0">Please select a suspect...</option>
+            ${
+                criminalsCollection.map(
+                    (criminal) => `
+                        <option value=${criminal.id}>${criminal.name}</option>
+                    `
+                )
+            }
+        </select>
         <button id="saveNote">Save Note</button>
     `
 }
 
 export const NoteForm = () => {
-    render()
+    getCriminals()
+    .then( () => render())
 }
--- scripts/notes/NoteList.js
+++ scripts/notes/NoteList.js
@@ -1,5 +1,6 @@
-import { getNotes, useNotes } from "./NoteDataProvider.js";
+import { getNotes, useNotes, deleteNote } from "./NoteDataProvider.js";
 import { NoteHTMLConverter } from "./Note.js";
+import { useCriminals } from "../criminals/CriminalDataProvider.js"
 
 const contentTarget = document.querySelector(".noteList")
 const eventHub = document.querySelector(".container")
@@ -12,17 +13,41 @@ eventHub.addEventListener("noteStateChanged", () => {
   NoteList()
 })
 
-const render = (noteArray) => {
-    const allNotesConvertedToStrings = noteArray.map((note) => NoteHTMLConverter(note)).join("")
+eventHub.addEventListener("click", clickEvent => {
+    if (clickEvent.target.id.startsWith("deleteNote--")) {
+        const [prefix, noteId] = clickEvent.target.id.split("--")
+
+        /*
+            Invoke the function that performs the delete operation.
+
+            Once the operation is complete you should THEN invoke
+            useNotes() and render the note list again.
+        */
+       deleteNote(noteId)
+    }
+})
+
+const render = (noteArray, criminals) => {
+    const allNotesConvertedToStrings = noteArray.map((note) => {
+        const associatedCriminal = criminals.find(
+            (criminal) => {
+                return criminal.id === note.criminalId
+            }
+        )
+        note.criminalName = associatedCriminal.name
+        
+        return NoteHTMLConverter(note)
+    }).join("")
 
     contentTarget.innerHTML = allNotesConvertedToStrings
 }
 
 
 export const NoteList = () => {
+    let criminals = useCriminals()
     getNotes()
         .then(() => {
             const allNotes = useNotes()
-            render(allNotes)
+            render(allNotes, criminals)
         })
 }
